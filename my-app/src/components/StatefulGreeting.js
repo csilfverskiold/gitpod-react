315c7f56146b194075dbbb7484fc2ad85ef9bbbb
csilfverskiold <christian.silfverskiold@gmail.com>
2023-02-03 10:47:17 +0000

    Add state inc event handler to StatefulGreeting component

--- my-app/src/components/StatefulGreeting.js
+++ my-app/src/components/StatefulGreeting.js
@@ -4,11 +4,76 @@ import React from "react";
 // 1.1 Extend class component through React ("extends React.Component")
 // 1.2 Define a render method and return 
 // 1.2 Pass in greeting prop in class (don't forget "this"!)
+//
+// 1.3 Add state to a class component: First define the constructor,  
+//     call super, then add the initial state object inside the constructor.
+//     Add constructor, a special method that is always called when the class is first instantiated.
+//     
+// 1.4.0 Add event handler to class component, example: Add onClick attribute, define handleClick method in
+//       class component, call setState method containing objects in handleClick method, 
+//       add callback function "() => {" after setState method to render properly in console. 
+//
+// 1.4.1 Add ternary condition to event handler, to change from "Hello! / Exit" to "Goodbye! / Enter"
+//       with === and ? :
+
+// 1.4.2 Continue to add third rule of updating the state, follow supplied the "Updating the State - Cheat Sheet"
+//
 class StatefulGreeting extends React.Component {
-    render() {
-        return <h1>Hello {this.props.name} {this.props.greeting}</h1>
+
+    constructor(props) {         //Always add contstructor this way
+        super(props);
+        this.state = {      //State object, can have many properties
+            introduction: "Hello!",
+            buttonText: "Exit",
+            count: 0
+        };
     }
-}
+
+    handleClick() {
+        this.setState((prevState, prevProps) => {
+          console.log("Previous State:", prevState);
+          console.log("Previous Props:", prevProps);
+          return {
+            introduction:
+              prevState.introduction === "Hello!" ? "Goodbye!" : "Hello!",
+            buttonText: prevState.buttonText === "Exit" ? "Enter" : "Exit",
+          };
+        });
+      }
+    
+      increment() {
+        this.setState((prevState, prevProps) => {
+          console.log("Previous State:", prevState);
+          console.log("Previous Props:", prevProps);
+          return {
+            count: prevState.count + 1,
+          };
+        });
+      }
+    
+      incrementFive() {
+        this.increment();
+        this.increment();
+        this.increment();
+        this.increment();
+        this.increment();
+      }
+    
+      render() {
+        return (
+          <div>
+            <h1>
+              {this.state.introduction} {this.props.greeting}
+            </h1>
+            <button onClick={() => this.handleClick()}>
+              {this.state.buttonText}
+            </button>
+            <button onClick={() => this.increment()}>Increment</button>
+            <p>You've clicked {this.state.count} times</p>
+          </div>
+        );
+      }
+    }
 
 // 2. Export class component
 export default StatefulGreeting;
