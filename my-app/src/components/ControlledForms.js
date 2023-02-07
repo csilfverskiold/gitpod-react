ca9e2d94a5ae7cf64688741cbcb7455ce88cabcd
csilfverskiold <christian.silfverskiold@gmail.com>
2023-02-07 13:38:03 +0000

    Edit ControlledForm component

--- my-app/src/components/ControlledForms.js
+++ my-app/src/components/ControlledForms.js
@@ -5,23 +5,30 @@ import React, { Component } from 'react'
 
 export class ControlledForms extends Component {
     constructor(props) {
-      super(props)
-    
-      this.state = {
-         name: ''
-      }
+        super(props)
+
+        this.state = {
+            name: ''
+        }
     }
-  render() {
-    return (
-      <div>
-        <form>
-            <label htmlFor="id-name">Your name: </label>
-            <input value={this.state.name} id="id-name" name="name" type="text" />
-            <input type="submit" value="Submit" />
-        </form>
-      </div>
-    )
-  }
+
+    handleNameChange = (event) => {
+        this.setState({
+            name: event.target.value
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <form>
+                    <label htmlFor="id-name">Your name: </label>
+                    <input value={this.state.name} onChange={this.handleNameChange} id="id-name" name="name" type="text" />
+                    <input type="submit" value="Submit" />
+                </form>
+            </div>
+        )
+    }
 }
 
 export default ControlledForms
@@ -34,3 +41,13 @@ export default ControlledForms
 // by adding a property to the state and setting the  value of the input equal to that state property.  
 // This causes a problem though, because  now that React is controlling the form,  
 // the input doesn’t seem to work anymore."
+
+// "Further, you’ve learned how  to complete the creation of a form  
+// with controlled input elements in React. Any other inputs you add to the form  
+// can be controlled in the exact same way. All  you need to do to create a controlled component  
+// is capture the onChange event from each form field you want React to control,  
+// and make sure that it has a matching property in the state so you can set its value accordingly.
+// Right now, the form fields in this form are controlled by React but the submission of the  
+// form is still handled by the DOM, which means when the user submits it, the page refreshes  
+// and the form data is lost. In the next lesson, we’ll add a couple more fields to this form  
+// and learn how to give React control over submitting the form as well."
